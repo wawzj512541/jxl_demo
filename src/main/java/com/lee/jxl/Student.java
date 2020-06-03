eb743587fd79aeca0a7e17e1b92a84928022f56d
andsen <wawzj512541@gmail.com>
2020-06-03 21:47:08 +0800

    首次提交

---
+++ src/main/java/com/lee/jxl/Student.java
@@ -0,0 +1,24 @@
+package com.lee.jxl;
+
+public class Student {
+
+    String name;
+    String sex;
+    String age;
+    String id;
+    String classNo;
+    String math;
+    String english;
+    String chinese;
+
+    public Student(String name, String sex, String age, String id, String classNo, String math, String english, String chinese) {
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+        this.id = id;
+        this.classNo = classNo;
+        this.math = math;
+        this.english = english;
+        this.chinese = chinese;
+    }
+}
